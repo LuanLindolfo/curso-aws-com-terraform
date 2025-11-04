766d77c4d2a6100885ce56654522715a1ef64fe0
Luan Lindolfo <136128927+LuanLindolfo@users.noreply.github.com>
2025-11-04 10:42:11 -0300

    Add comments for clarity in locals.tf
    Added comments to clarify local variables in locals.tf.

--- 03-site-estatico/terraform/locals.tf
+++ 03-site-estatico/terraform/locals.tf
@@ -1,13 +1,13 @@
 locals {
-  has_domain       = var.domain != ""
-  domain           = local.has_domain ? var.domain : random_pet.website.id
-  regional_domain  = module.website.regional_domain_name
-  website_filepath = "${path.module}/../website"
+  has_domain       = var.domain != ""      # Indica se foi informado um domínio customizado.
+  domain           = local.has_domain ? var.domain : random_pet.website.id # Usa domínio customizado ou gera aleatoriamente.
+  regional_domain  = module.website.regional_domain_name # Endereço do bucket S3 principal.
+  website_filepath = "${path.module}/../website"         # Caminho local para arquivos do site.
 
   common_tags = {
-    Project   = "Curso AWS com Terraform"
-    Service   = "Static Website"
-    CreatedAt = "2020-03-14"
-    Module    = "3"
+    Project   = "Curso AWS com Terraform"  # Tag de projeto.
+    Service   = "Static Website"           # Tag de serviço.
+    CreatedAt = "2020-03-14"               # Data de criação.
+    Module    = "3"                        # Módulo identificador.
   }
 }
