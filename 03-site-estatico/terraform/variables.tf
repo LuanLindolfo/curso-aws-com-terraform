e9f64e25e418f42053682eae7aa0e65958f0c122
Luan Lindolfo <136128927+LuanLindolfo@users.noreply.github.com>
2025-11-04 10:43:34 -0300

    Add descriptions to AWS variables in Terraform

--- 03-site-estatico/terraform/variables.tf
+++ 03-site-estatico/terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
   type        = string
-  description = ""
+  description = "Região padrão da AWS"
   default     = "eu-central-1"
 }
 
 variable "aws_profile" {
   type        = string
-  description = ""
+  description = "Perfil AWS CLI para executar comandos Terraform"
   default     = "tf014"
 }
 
 variable "domain" {
   type        = string
-  description = ""
+  description = "Domínio customizado do site. Se vazio, um nome é gerado aleatoriamente."
   default     = ""
 }
